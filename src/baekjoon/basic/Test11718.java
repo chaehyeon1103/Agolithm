ca21d8f0b205b694f8ce675f3d2f9b5b1b6169c1
chaehyeon <cogus1103@naver.com>
2022-09-30 13:05:30 +0900

    새싹 문제 - 220930

---
+++ src/baekjoon/basic/Test11718.java
@@ -0,0 +1,12 @@
+package baekjoon.basic;
+
+import java.util.Scanner;
+
+public class Test11718 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) System.out.println(sc.nextLine());
+    }
+}
--- src/baekjoon/basic/Test9086.java
+++ src/baekjoon/basic/Test9086.java
@@ -6,9 +6,11 @@ public class Test9086 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < sc.nextInt(); i++) {
-            String str = sc.nextLine();
-            System.out.println(str.valueOf(0) + str.valueOf(str.length()));
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            String str = sc.next();
+            System.out.println(String.valueOf(str.charAt(0)) + String.valueOf(str.charAt(str.length()-1)));
+            // char to string 변환 -> String.. = String.valueOf(char..)
         }
     }
 }
